bd1dbfbcca9c89e987441cc07ee2d2ce7c5283de
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2021-04-05 16:35:49 -0500

    adding more test methods

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import augmentor
 import os
+import numpy as np
+from matplotlib import pyplot as plt
+import cv2
 
 
 def get_directory_contents(directory_path):
@@ -33,19 +36,73 @@ def test_load(image_file_paths):
 def test_get_corners(images):
     """
     Test method for the _get_corners method of the augmentor class. gets the coordinates of the corners of the images.
+
     :param images: The images to get the corners of.
     :return: list of the corners of the images
     """
-    out = []
+    corners = []
     for image in images:
-        out.append(augmentor._get_corners(image))
+        corners.append(augmentor._get_corners(image))
 
-    return out
+    return corners
+
+
+def test__get_rand_points():
+    """
+    Test method for the _get_rand_points method of the augmentor class.
+
+    :return: List of random points.
+    """
+    points = []
+    for i in range(3):
+        point = augmentor._get_rand_points(low=i, high=i + 1, shape=[i + 1, i + 1])
+        points.append(point)
+
+    return points
+
+
+def test_perspective_transformation(images):
+    """
+    Test method for the perspective_transformation method of the augmentor class.
+
+    :param images: list of images to transform.
+    :return: Images with a perspective transformation performed.
+    """
+    transformations = []
+    kernel = np.array([[1, 0, 300], [0, 1, 150], [0, 0, 1]]).astype('float32')
+    for image in images:
+        transformed = augmentor.perspective_transformation(image, kernel)
+        transformations.append(transformed)
+
+    return transformations
+
+
+def test_warp_corners(images):
+    """
+    Test method for the warp_corners method of the augmentor class.
+
+    :param images: The images to transform.
+    :return: Images with the corners warped.
+    """
+    transformations = []
+    for image in images:
+        transformed = augmentor.warp_corners(image, augmentor._get_rand_points(0, 0.25, [4]),
+                                             augmentor._get_rand_points(0, 0.25, [4]))
+        transformations.append(transformed)
+
+    return transformations
 
 
 #####################################
 # Helper formatting methods
 #####################################
+
+def announcment(method):
+    annc = "# Testing {}".format(method)
+    print("#" * len(annc))
+    print(annc)
+    print("#" * len(annc), end="\n\n")
+
 
 def get_images(path):
     image_file_paths = get_directory_contents(path)
@@ -54,11 +111,13 @@ def get_images(path):
     for path in image_file_paths:
         print("\t{}".format(path))
 
+    print()
+
     return image_file_paths
 
 
 def load_images(image_file_paths):
-    print()
+    announcment("load")
     print("Loading the images...")
     images = test_load(image_file_paths)
     print("Done", end="\n\n")
@@ -67,9 +126,35 @@ def load_images(image_file_paths):
 
 
 def corners(images):
+    announcment("_get_corners")
     corners = test_get_corners(images)
     for corner in corners:
         print("found corner:\n{}".format(corner), end="\n\n")
+
+
+def points():
+    announcment("_get_rand_points")
+    points = test__get_rand_points()
+    for point in points:
+        print("Created point:\n{}".format(point), end="\n\n")
+
+
+def perspective_transform(images, show=True):
+    announcment("perspective_transformation")
+    images = test_perspective_transformation(images)
+    if show:
+        for image in images:
+            plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            plt.show()
+
+
+def warp(images, show=True):
+    announcment("warp_corners")
+    images = test_warp_corners(images)
+    if show:
+        for image in images:
+            plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+            plt.show()
 
 
 if __name__ == '__main__':
@@ -79,8 +164,8 @@ if __name__ == '__main__':
     #######
     # TESTS
     #######
-    print("############")
-    print("# PERFORMING TESTS")
-    print("############", end="\n\n")
 
     corners(images)
+    points()
+    perspective_transform(images)
+    warp(images)
